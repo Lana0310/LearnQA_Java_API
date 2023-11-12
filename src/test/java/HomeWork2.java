a41a02f980724b9a32a850874613d296f55bb396
Lana0310 <lanazsn0310@gmail.com>
2023-11-13 01:40:15 +0300

    Ex6: Редирект

--- src/test/java/HomeWork2.java
+++ src/test/java/HomeWork2.java
@@ -1,6 +1,9 @@
 import io.restassured.RestAssured;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
 import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
+
 
 public class HomeWork2 {
     @Test
@@ -11,5 +14,17 @@ public class HomeWork2 {
                 .jsonPath();
         System.out.println(response.get("messages.message[1]").toString());
         System.out.println(response.get("messages.timestamp[1]").toString());
+    }
+    @Test
+    public void TestRedirect() {
+        Response response= RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+        String location=response.getHeader("location");
+        System.out.println(location);
     }
 }
