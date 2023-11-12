f802c63ce631587556b48b2859a158d7a61a8924
Lana0310 <lanazsn0310@gmail.com>
2023-11-13 01:54:00 +0300

    Ex7: Долгий редирект

--- src/test/java/HomeWork2.java
+++ src/test/java/HomeWork2.java
@@ -27,4 +27,25 @@ public class HomeWork2 {
         String location=response.getHeader("location");
         System.out.println(location);
     }
+    @Test
+    public void TestLongRedirect() {
+        int statusCode=0;
+        int count=0;
+        String location="https://playground.learnqa.ru/api/long_redirect";
+        while (statusCode!=200){
+            Response response = RestAssured
+                    .given()
+                    .redirects()
+                    .follow(false)
+                    .when()
+                    .get(location)
+                    .andReturn();
+            location = response.getHeader("location");
+            statusCode = response.getStatusCode();
+            if(statusCode!=200) count++;
+            System.out.println(location);
+            System.out.println(statusCode);
+        }
+        System.out.println("Колличество редиректов "+count);
+    }
 }
