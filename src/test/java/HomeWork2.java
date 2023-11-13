e6496a61be5a741afec0fdc247c9f6052f227d82
Lana0310 <lanazsn0310@gmail.com>
2023-11-13 23:30:12 +0300

    Ex8: Токены

--- src/test/java/HomeWork2.java
+++ src/test/java/HomeWork2.java
@@ -3,6 +3,9 @@ import io.restassured.http.Headers;
 import io.restassured.response.Response;
 import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
+import java.lang.Thread;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class HomeWork2 {
@@ -47,5 +50,42 @@ public class HomeWork2 {
             System.out.println(statusCode);
         }
         System.out.println("Колличество редиректов "+count);
+    }
+    
+    @Test
+    public void TestToken() {
+        JsonPath responseGetToken= RestAssured
+                .given()
+                .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                .jsonPath();
+        String token=responseGetToken.get("token").toString();
+        int time=responseGetToken.get("seconds");
+        int delay=time*1000;
+        Map<String,String> params = new HashMap<>();
+        params.put("token",token);
+        JsonPath responseNotReady=RestAssured
+                .given()
+                .queryParams(params)
+                .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                .jsonPath();
+        responseNotReady.prettyPrint();
+        if(responseNotReady.get("status").equals("Job is NOT ready")){
+            try{
+                Thread.sleep(delay);
+            }
+            catch (Exception e){
+                System.out.println(e);
+            }
+            JsonPath responseGetResult=RestAssured
+                    .given()
+                    .queryParams(params)
+                    .get("https://playground.learnqa.ru/ajax/api/longtime_job")
+                    .jsonPath();
+            if(responseGetResult.get("result")!=null&&responseGetResult.get("status").equals("Job is ready")) {
+                responseGetResult.prettyPrint();
+            }
+            else System.out.println("Error");
+
+        }
     }
 }
