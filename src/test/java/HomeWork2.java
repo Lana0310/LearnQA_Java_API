eb10d913e1d9c97138eca0a0749c34b045a942a6
Lana0310 <lanazsn0310@gmail.com>
2023-11-21 00:33:22 +0300

    Ex13: User Agent

--- src/test/java/HomeWork2.java
+++ src/test/java/HomeWork2.java
@@ -5,6 +5,7 @@ import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.lang.Thread;
@@ -151,5 +152,24 @@ public class HomeWork2 {
         Headers headers=responseGetAuth.getHeaders();
         assertTrue(headers.hasHeaderWithName("x-secret-homework-header"),"Response doesn't have  header 'x-csrf-token header'");
         assertEquals("Some secret value", headers.getValue("x-secret-homework-header"), "Unexpected value for header 'x-secret-homework-header'");
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "Mobile, No, Android, 'Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30'",
+            "Mobile, Chrome, iOS, 'Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1'",
+            "Googlebot, Unknown, Unknown, 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'",
+            "Web, Chrome, No, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0'",
+            "Mobile, No, iPhone,'Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'"
+    })
+    public void testForUserAgent(String platform,String browser,String device,String userAgent){
+        Response response = RestAssured
+                .given()
+                .header("User-Agent",userAgent)
+                .get("https://playground.learnqa.ru/ajax/api/user_agent_check")
+                .andReturn();
+        assertEquals(platform,response.jsonPath().get("platform"),"Response value platform doesn't equal "+platform);
+        assertEquals(browser,response.jsonPath().get("browser"),"Response value browser doesn't equal "+browser);
+        assertEquals(device,response.jsonPath().get("device"),"Response value device doesn't equal "+device);
     }
 }
